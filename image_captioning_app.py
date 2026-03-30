04b5872c137caf3c9c6313f1c542c99bd3294dff
nurdan-VisibleEdge <nurdan.saban@vei-edu.com>
2026-03-30 14:45:40 -0400

    first commit

---
+++ image_captioning_app.py
@@ -0,0 +1,24 @@
+import gradio as gr
+import numpy as np
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+def caption_image(input_image: np.ndarray):
+    raw_image = Image.fromarray(input_image).convert("RGB")
+    inputs = processor(raw_image, return_tensors="pt")
+    out = model.generate(**inputs, max_length=50)
+    caption = processor.decode(out[0], skip_special_tokens=True)
+    return caption
+
+iface = gr.Interface(
+    fn=caption_image,
+    inputs=gr.Image(type="numpy"),
+    outputs="text",
+    title="Image Captioning",
+    description="Upload an image and get a caption."
+)
+
+iface.launch()
